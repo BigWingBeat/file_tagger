a3d6e321279c8fb02cd61e28dd0df44bf5189cc0
BigWingBeat <bigwingbeat@siliconwings.net>
2026-09-02 00:48:53 +0100

    Make it smallvec

--- file_tagger_internals/src/sortedset.rs
+++ file_tagger_internals/src/sortedset.rs
@@ -5,57 +5,61 @@ use std::{
     slice::SliceIndex,
 };
 
-/// A bit like a `BTreeSet`, but backed by a single sorted `Vec<T>` instead of a tree of nodes.
+use smallvec::SmallVec;
+
+/// A bit like a `BTreeSet`, but backed by a single sorted `SmallVec<[T; N]>` instead of a tree of nodes.
 /// This makes it simpler, and faster to construct and read from, in exchange for mutations being slower.
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
-pub struct SortedSet<T> {
-    vec: Vec<T>,
+pub struct SmallSortedSet<T, const N: usize> {
+    vec: SmallVec<[T; N]>,
 }
 
-impl<T> SortedSet<T> {
+impl<T, const N: usize> SmallSortedSet<T, N> {
     #[inline]
     pub const fn new() -> Self {
-        Self { vec: Vec::new() }
+        Self {
+            vec: SmallVec::new_const(),
+        }
     }
 
     #[inline]
     pub fn with_capacity(capacity: usize) -> Self {
         Self {
-            vec: Vec::with_capacity(capacity),
+            vec: SmallVec::with_capacity(capacity),
         }
     }
 
-    /// Consume `self` and return the sorted inner `Vec`
+    /// Consume `self` and return the sorted inner `SmallVec`
     #[inline]
-    pub fn into_vec(self) -> Vec<T> {
+    pub fn into_vec(self) -> SmallVec<[T; N]> {
         self.vec
     }
 
-    /// See [`Vec::reserve`]
+    /// See [`SmallVec::reserve`]
     #[inline]
     pub fn reserve(&mut self, additional: usize) {
         self.vec.reserve(additional);
     }
 
-    /// See [`Vec::reserve_exact`]
+    /// See [`SmallVec::reserve_exact`]
     #[inline]
     pub fn reserve_exact(&mut self, additional: usize) {
         self.vec.reserve_exact(additional);
     }
 
-    /// See [`Vec::shrink_to_fit`]
+    /// See [`SmallVec::shrink_to_fit`]
     #[inline]
     pub fn shrink_to_fit(&mut self) {
         self.vec.shrink_to_fit();
     }
 
-    /// See [`Vec::shrink_to`]
+    /// Returns the number of items the collection can hold without reallocating.
     #[inline]
-    pub fn shrink_to(&mut self, min_capacity: usize) {
-        self.vec.shrink_to(min_capacity);
+    pub fn capacity(&self) -> usize {
+        self.vec.capacity()
     }
 
-    // We have an inherent method for this, rather than forwarding to [`Slice::len`] via `Deref`, because `Vec` does too I guess
+    // We have an inherent method for this, rather than forwarding to [`Slice::len`] via `Deref`, because `SmallVec` does too I guess
     /// Returns the number of elements in the collection.
     #[inline]
     pub fn len(&self) -> usize {
@@ -100,17 +104,18 @@ impl<T> SortedSet<T> {
     }
 
     /// Removes a range of elements, returning a double-ended iterator over the removed subslice.
-    /// See: [`Vec::drain`]
+    /// See: [`SmallVec::drain`]
     #[inline]
-    pub fn drain(&mut self, range: impl RangeBounds<usize>) -> std::vec::Drain<'_, T> {
+    pub fn drain(&mut self, range: impl RangeBounds<usize>) -> smallvec::Drain<'_, [T; N]> {
         self.vec.drain(range)
     }
 
     /// Retains only the elements for which `F` returns `true`, removing all other elements.
-    /// See: [`Vec::retain`]
+    /// See: [`SmallVec::retain`]
     #[inline]
-    pub fn retain(&mut self, f: impl FnMut(&T) -> bool) {
-        self.vec.retain(f)
+    pub fn retain(&mut self, mut f: impl FnMut(&T) -> bool) {
+        // For some stupid reason `SmallVec::retain` passes `&mut T` instead of `&T` despite also having a separate `retain_mut` method
+        self.vec.retain(|t| f(t))
     }
 
     /// Directly inserts the given element at the given index, without checking for correct sort order.
@@ -125,27 +130,34 @@ impl<T> SortedSet<T> {
         self.vec.insert(index, element)
     }
 
-    /// Directly inserts the given element at the given index, without checking for correct sort order.
-    /// Returns a reference to the new element.
-    ///
-    /// # Safety
-    /// The collection must still be sorted after the given element is inserted at the given index.
-    ///
-    /// # Panic
-    /// Panics if the given index is out of bounds.
-    #[must_use = "if you don't need a reference to the value, use `SortedSet::insert_at` instead"]
-    #[inline]
-    pub unsafe fn insert_at_mut(&mut self, index: usize, element: T) -> &mut T {
-        self.vec.insert_mut(index, element)
-    }
+    // Waiting on SmallVec to update to add this method
+    // /// Directly inserts the given element at the given index, without checking for correct sort order.
+    // /// Returns a reference to the new element.
+    // ///
+    // /// # Safety
+    // /// The collection must still be sorted after the given element is inserted at the given index.
+    // ///
+    // /// # Panic
+    // /// Panics if the given index is out of bounds.
+    // #[must_use = "if you don't need a reference to the value, use `SortedSet::insert_at` instead"]
+    // #[inline]
+    // pub unsafe fn insert_at_mut(&mut self, index: usize, element: T) -> &mut T {
+    //     self.vec.insert_mut(index, element)
+    // }
 }
 
-impl<T: Ord> SortedSet<T> {
+impl<T: Ord, const N: usize> SmallSortedSet<T, N> {
     /// Sorts and deduplicates the given elements
-    pub fn from_unsorted(mut vec: Vec<T>) -> Self {
+    pub fn from_unsorted(mut vec: SmallVec<[T; N]>) -> Self {
         vec.sort_unstable();
         vec.dedup();
         Self { vec }
+    }
+
+    /// Sorts and deduplicates the given elements
+    #[inline]
+    pub fn from_unsorted_vec(vec: Vec<T>) -> Self {
+        Self::from_unsorted(vec.into())
     }
 
     // We have an inherent method for this, rather than forwarding to [`Slice::contains`] via `Deref`, to avoid the linear scan
@@ -181,40 +193,45 @@ impl<T: Ord> SortedSet<T> {
     }
 }
 
-impl<T: Ord + Clone> SortedSet<T> {
+impl<T: Ord + Clone, const N: usize> SmallSortedSet<T, N> {
     /// Clones, sorts, and deduplicates the given elements
+    #[inline]
     pub fn from_unsorted_slice(slice: &[T]) -> Self {
-        let mut vec = slice.to_vec();
-        vec.sort_unstable();
-        vec.dedup();
-        Self { vec }
+        Self::from_unsorted(slice.into())
     }
 }
 
-impl<T> Default for SortedSet<T> {
+impl<T, const N: usize> Default for SmallSortedSet<T, N> {
     #[inline]
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T: Ord> From<Vec<T>> for SortedSet<T> {
+impl<T: Ord, const N: usize> From<SmallVec<[T; N]>> for SmallSortedSet<T, N> {
     #[inline]
-    fn from(unsorted: Vec<T>) -> Self {
+    fn from(unsorted: SmallVec<[T; N]>) -> Self {
         Self::from_unsorted(unsorted)
     }
 }
 
-impl<T: Ord + Clone> From<&[T]> for SortedSet<T> {
+impl<T: Ord, const N: usize> From<Vec<T>> for SmallSortedSet<T, N> {
+    #[inline]
+    fn from(unsorted: Vec<T>) -> Self {
+        Self::from_unsorted_vec(unsorted)
+    }
+}
+
+impl<T: Ord + Clone, const N: usize> From<&[T]> for SmallSortedSet<T, N> {
     #[inline]
     fn from(unsorted: &[T]) -> Self {
         Self::from_unsorted_slice(unsorted)
     }
 }
 
-// Like with std `Vec`, this provides a lot of useful methods that we would otherwise have to impl ourselves.
-// Note that unlike std `Vec`, we do not also impl `DerefMut`, as that would allow violating both of our invariants.
-impl<T> Deref for SortedSet<T> {
+// Like with `SmallVec`, this provides a lot of useful methods that we would otherwise have to impl ourselves.
+// Note that unlike `SmallVec`, we do not also impl `DerefMut`, as that would allow violating both of our invariants.
+impl<T, const N: usize> Deref for SmallSortedSet<T, N> {
     type Target = [T];
 
     #[inline]
@@ -224,7 +241,7 @@ impl<T> Deref for SortedSet<T> {
 }
 
 // Ditto for not also implementing `AsMut`
-impl<T> AsRef<[T]> for SortedSet<T> {
+impl<T, const N: usize> AsRef<[T]> for SmallSortedSet<T, N> {
     #[inline]
     fn as_ref(&self) -> &[T] {
         &self.vec
@@ -232,7 +249,7 @@ impl<T> AsRef<[T]> for SortedSet<T> {
 }
 
 // Ditto ditto for not also implementing `BorrowMut`
-impl<T> Borrow<[T]> for SortedSet<T> {
+impl<T, const N: usize> Borrow<[T]> for SmallSortedSet<T, N> {
     #[inline]
     fn borrow(&self) -> &[T] {
         &self.vec
@@ -240,7 +257,7 @@ impl<T> Borrow<[T]> for SortedSet<T> {
 }
 
 // Ditto ditto ditto for not also implementing `IndexMut`
-impl<T, I: SliceIndex<[T]>> Index<I> for SortedSet<T> {
+impl<T, I: SliceIndex<[T]>, const N: usize> Index<I> for SmallSortedSet<T, N> {
     type Output = I::Output;
 
     #[inline]
@@ -249,7 +266,7 @@ impl<T, I: SliceIndex<[T]>> Index<I> for SortedSet<T> {
     }
 }
 
-impl<A: Ord> Extend<A> for SortedSet<A> {
+impl<A: Ord, const N: usize> Extend<A> for SmallSortedSet<A, N> {
     fn extend<T: IntoIterator<Item = A>>(&mut self, iter: T) {
         self.vec.extend(iter);
         // Prefer stable sort to unstable sort here as we know the extended vec is already sorted up to the new elements
@@ -258,16 +275,16 @@ impl<A: Ord> Extend<A> for SortedSet<A> {
     }
 }
 
-impl<A: Ord> FromIterator<A> for SortedSet<A> {
+impl<A: Ord, const N: usize> FromIterator<A> for SmallSortedSet<A, N> {
     fn from_iter<T: IntoIterator<Item = A>>(iter: T) -> Self {
-        let vec = Vec::from_iter(iter);
+        let vec = SmallVec::from_iter(iter);
         Self::from_unsorted(vec)
     }
 }
 
-impl<T> IntoIterator for SortedSet<T> {
+impl<T, const N: usize> IntoIterator for SmallSortedSet<T, N> {
     type Item = T;
-    type IntoIter = std::vec::IntoIter<T>;
+    type IntoIter = smallvec::IntoIter<[T; N]>;
 
     #[inline]
     fn into_iter(self) -> Self::IntoIter {
@@ -275,7 +292,7 @@ impl<T> IntoIterator for SortedSet<T> {
     }
 }
 
-impl<'a, T> IntoIterator for &'a SortedSet<T> {
+impl<'a, T, const N: usize> IntoIterator for &'a SmallSortedSet<T, N> {
     type Item = &'a T;
     type IntoIter = std::slice::Iter<'a, T>;
 
@@ -285,7 +302,7 @@ impl<'a, T> IntoIterator for &'a SortedSet<T> {
     }
 }
 
-impl<T: Hash> Hash for SortedSet<T> {
+impl<T: Hash, const N: usize> Hash for SmallSortedSet<T, N> {
     #[inline]
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.vec.hash(state);
@@ -294,26 +311,31 @@ impl<T: Hash> Hash for SortedSet<T> {
 
 #[cfg(test)]
 mod tests {
+    use smallvec::smallvec_inline;
+
     use super::*;
 
     #[test]
     fn sorted_set() {
-        let mut s = SortedSet::new();
+        let mut s = SmallSortedSet::<_, 3>::new();
         assert_eq!(s.insert(5), Ok(0));
         assert_eq!(s.insert(3), Ok(0));
         assert_eq!(s.insert(4), Ok(1));
         assert_eq!(s.insert(4), Err(1));
         assert_eq!(s.len(), 3);
         assert_eq!(s.binary_search(&3), Ok(0));
+
         assert_eq!(
-            *SortedSet::from_unsorted(vec![5, -10, 99, -10, -11, 10, 2, 17, 10]),
+            *SmallSortedSet::from_unsorted(smallvec_inline![5, -10, 99, -10, -11, 10, 2, 17, 10]),
             vec![-11, -10, 2, 5, 10, 17, 99]
         );
+
         assert_eq!(
-            SortedSet::from_unsorted(vec![5, -10, 99, -10, -11, 10, 2, 17, 10]),
+            SmallSortedSet::from_unsorted(smallvec_inline![5, -10, 99, -10, -11, 10, 2, 17, 10]),
             vec![5, -10, 99, -10, -11, 10, 2, 17, 10].into()
         );
-        let mut s = SortedSet::new();
+
+        let mut s = SmallSortedSet::<_, 7>::new();
         s.extend([5, -11, -10, 99, -11, 2, 17, 2, 10]);
         assert_eq!(*s, vec![-11, -10, 2, 5, 10, 17, 99]);
         s.remove_at(0);
